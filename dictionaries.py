7e699cfacef650ddcc6511f43a359c8839e0fbb3
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-16 22:32:03 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,27 @@
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict)
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# x = thisdict["model"]
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
